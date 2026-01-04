157b91cb867d67d2c560fc7d0b689d75982c6774
Pachev_Danila <pachev-danila@mail.ru>
2026-01-04 23:34:37 +0300

    feat: implement pagination for product listing and highlight active catalog link

--- shop/catalog/views.py
+++ shop/catalog/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import ProductVariant, Category
+from django.core.paginator import Paginator
 
 def catalog(request):
     products= ProductVariant.objects.select_related('product','color','category').all()
     categories = Category.objects.all()
-    return render(request,'catalog/catalog.html',{'products':products, 'categories':categories})
+    paginator = Paginator(products, 9)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request,'catalog/catalog.html',{'categories':categories, 'page_obj':page_obj})
